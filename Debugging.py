928dec9dc55d2e72ec3f2f20eeecccb7bacad077
pugazh2006 <pugazhenthij283@gmail.com>
2025-09-07 09:05:14 +0530

    banner colour change

--- Debugging.py
+++ Debugging.py
@@ -3,21 +3,25 @@ import subprocess
 import shutil
 from typing import Optional, Tuple
 
-
 def banner():
-    print(r"""
+    GREEN = "\033[92m"
+    BLUE = "\033[94m"
+    RESET = "\033[0m"
+
+    print(GREEN + r"""
       ___           _                _ 
      / _ \__ _  ___| |__   __ _  ___| |
     / /_)/ _` |/ __| '_ \ / _` |/ _ \ |
    / ___/ (_| | (__| | | | (_| |  __/ |
    \/    \__,_|\___|_| |_|\__, |\___|_|
                           |___/        
+    """ + RESET)
 
-        Android Wireless Debugging Tool
-        Developed by: PugazhTheHacker
-        GitHub: https://github.com/PugazhTheHacker
-    """)
+    print(BLUE + "        Android Wireless Debugging Tool" + RESET)
+    print(BLUE + "        Developed by: PugazhTheHacker" + RESET)
+    print(BLUE + "     GitHub: https://github.com/PugazhTheHacker")
     print("=" * 60)
+
 
 
 def which(cmd: str) -> Optional[str]:
